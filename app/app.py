d7083a452232fa8671c6ba5924796f1c9d86722a
John Li <johnli8642@gmail.com>
2024-10-17 16:58:18 -0400

    update template to have style section

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ def index():
     if request.method == 'POST':
         user_input = request.form['user_input']
         return render_template('index.html', user_input=user_input)
-    return render_template('index.html', user_input=None)
+    return render_template('testpage.html', user_input=None)
 
 if __name__ == '__main__':
     # app.run(debug=True)
