1c0521a63ef0b96fa4b49df2745b5802d24b3af2
// <embyrne02@gmail.com>
2024-10-19 18:35:00 -0400

    add list structure to table, link inv-monitoring page in the sidebar

--- app/app.py
+++ app/app.py
@@ -9,6 +9,16 @@ def index():
         return render_template('index.html', user_input=user_input)
     return render_template('testpage.html', user_input=None)
 
+@app.route('/inv-monitoring', methods=['GET', 'POST'])
+def drug_table():
+    # list of drugs to be displayed on inventory monitoring page
+    # hard coded for now but will be changed later
+    drugs = [
+        {"name": "Drug1", "company": "C1", "type": "Prescription", "stock": 20},
+        {"name": "Drug2", "company": "C2", "type": "Over the Counter", "stock": 30},
+    ]
+    return render_template('inv-monitoring.html', drugs=drugs)
+
 if __name__ == '__main__':
     # app.run(debug=True)
     app.run(port=5001, debug=True)
