b394c8ca646bc3e763a1b0103b8b683963470f38
KungPaoMeow <johnli8642@gmail.com>
2024-10-20 17:03:29 -0400

    rename  folder to , and update requirements so unittest works with Flask

--- flask_app/tests/test_unit_tests.py
+++ flask_app/tests/test_unit_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from app import app
 
 class TestTesting(unittest.TestCase):
     def test_testing_false(self):
@@ -18,6 +19,18 @@ class TestTesting2(unittest.TestCase):
     def test_test(self):
         self.assertTrue(False, "should be true")
 
+class TestFlaskApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+        print(self.app)
+        print(app)
+    
+    def test_index(self):
+        response = self.app.get('/')
+
+        # Check that the status code is 200 OK
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
