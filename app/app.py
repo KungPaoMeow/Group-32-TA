ca06239df6455d34eba1926c4ed2505d88a29a84
// <embyrne02@gmail.com>
2024-10-19 21:40:21 -0400

    add functionality for editing drugs

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+drugs = [
+        {"name": "Drug1", "company": "C1", "type": "Prescription", "stock": 20},
+        {"name": "Drug2", "company": "C2", "type": "Over the Counter", "stock": 30},
+    ]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -9,15 +14,27 @@ def index():
         return render_template('index.html', user_input=user_input)
     return render_template('testpage.html', user_input=None)
 
-@app.route('/inv-monitoring', methods=['GET', 'POST'])
+@app.route('/inv-monitoring')
 def drug_table():
     # list of drugs to be displayed on inventory monitoring page
     # hard coded for now but will be changed later
-    drugs = [
-        {"name": "Drug1", "company": "C1", "type": "Prescription", "stock": 20},
-        {"name": "Drug2", "company": "C2", "type": "Over the Counter", "stock": 30},
-    ]
+    
     return render_template('inv-monitoring.html', drugs=drugs)
+
+@app.route('/drug-edit/<int:id>', methods=['GET', 'POST'])
+def drug_edit(id):
+    # get the info of the selected drug
+    drug = drugs[id]
+    if request.method == 'POST':
+        # update the drug info
+        drug['name'] = request.form['name']
+        drug['company'] = request.form['company']
+        drug['type'] = request.form['type']
+        drug['stock'] = request.form['stock']
+        return redirect('/inv-monitoring')
+    # display the drug info
+    return render_template('drug-edit.html', drug=drug, ids=range(len(drugs)))
+    
 
 if __name__ == '__main__':
     # app.run(debug=True)
