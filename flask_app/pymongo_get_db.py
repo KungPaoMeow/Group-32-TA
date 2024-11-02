3e7960d6bbb8242628e9393a2ddbab7a0e18ca32
KungPaoMeow <johnli8642@gmail.com>
2024-11-02 12:44:01 -0400

    add db connection string so TA's can run project

--- flask_app/pymongo_get_db.py
+++ flask_app/pymongo_get_db.py
@@ -1,14 +1,17 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 
 
 class MongoDB:
     def __init__(self) -> None:
-        load_dotenv()
+        # load_dotenv()
         # Provide the mongodb atlas url to connect python to mongodb using pymongo
-        uri = os.getenv('DB_CONN_STRING')
+        # uri = os.getenv('DB_CONN_STRING')
+
+        # not best practice but needed
+        uri = "mongodb+srv://readwrite:FB6oj8w3dl8E1woi@dev-pharmacy-system-0.pdnj3.mongodb.net/?retryWrites=true&w=majority&appName=dev-pharmacy-system-0"
 
         # Create a new client and connect to the server
         client = MongoClient(uri, server_api=ServerApi('1'))
