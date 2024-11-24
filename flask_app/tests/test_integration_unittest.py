e4e85dae0cb88e38ea58ff2241115a1a1bd4c20d
// <embyrne02@gmail.com>
2024-11-24 03:39:43 -0500

    Update test_integration_unittest.py
    update editing drug and order tests to check that the data is present on all pages that should display the data

--- flask_app/tests/test_integration_unittest.py
+++ flask_app/tests/test_integration_unittest.py
@@ -104,6 +104,7 @@ class IntegrationTest(unittest.TestCase):
             db_service.db["orders"].delete_one({"_id": inserted_order_id})
     
     def test_edit_drug(self):
+        # create a sample drug to add to the database
         sample_drug = {
             "name": "Old Drug",
             "company": "Old Company",
@@ -112,8 +113,10 @@ class IntegrationTest(unittest.TestCase):
             "stock": 20
         }
 
+        # insert the drug into the db
         inserted_drug_id = db_service.db["drugs"].insert_one(sample_drug).inserted_id
         
+        # create a new updated drug to replace the drug in the db
         updated_drug = {
             "name": "New Drug",
             "company": "New Company",
@@ -122,21 +125,55 @@ class IntegrationTest(unittest.TestCase):
             "stock": 19
         }
 
+        # edit the drug via post request, follow the redirects to get to a page which displays the new drug
         response = self.app.post(f'/drug-edit/{inserted_drug_id}', data=updated_drug, follow_redirects = True)
 
+        # assert that the redirect was successful
         self.assertEqual(response.status_code, 200)
 
+        # get the updated drug from the db
         updated_drug = db_service.db["drugs"].find_one({"_id": inserted_drug_id})
 
+        # assert that the updated drug has the values defined earlier
         self.assertEqual(updated_drug["name"], "New Drug")
         self.assertEqual(updated_drug["company"], "New Company")
         self.assertEqual(updated_drug["type"], "Over-the-counter")
         self.assertEqual(updated_drug["description"], "New description")
         self.assertEqual(updated_drug["stock"], 19)
 
+        # check the inventory monitoring page to see if the sample drug is present
+        response = self.app.get('/inv-monitoring')
+        # assert the get request was successful
+        self.assertEqual(response.status_code, 200)
+
+        # check if the updated sample drug is present
+        self.assertIn(b"New Drug", response.data)
+        self.assertIn(b"New Company", response.data)
+        self.assertIn(b"Over-the-counter", response.data)
+        # the description isn't displayed on inv-monitoring page
+        self.assertIn(b"19", response.data)
+
+        # check the browse drugs page in the same way
+        response = self.app.get('/browse-drug')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"New Drug", response.data)
+        self.assertIn(b"New Company", response.data)
+        self.assertIn(b"Over-the-counter", response.data)
+        # the description isn't displayed on browse-drug page
+        self.assertIn(b"19", response.data)
+
+        # check the drug info page in the same way
+        response = self.app.get('/drug-info')
+        self.assertEqual(response.status_code, 200)
+        # only the drug name and description are present on the drug-info page
+        self.assertIn(b"New Drug", response.data)
+        self.assertIn(b"New description", response.data)
+
+        # remove the sample drug from the db to avoid filling up the db with vacuous data
         db_service.db["drugs"].delete_one({"_id": inserted_drug_id})
 
     def test_edit_order(self):
+        # create a sample order to add to the database
         sample_order = {
             "name": "Old Customer",
             "date_of_purchase": "2024-11-25",
@@ -144,8 +181,10 @@ class IntegrationTest(unittest.TestCase):
             "status": "pending"
         }
 
+        # insert the order into the db
         inserted_order_id = db_service.db["orders"].insert_one(sample_order).inserted_id
         
+        # create a new updated order to replace the order in the db
         updated_order = {
             "name": "New Customer",
             "date_of_purchase": "2024-11-26",
@@ -153,17 +192,41 @@ class IntegrationTest(unittest.TestCase):
             "status": "picked up"
         }
 
+        # edit the order via post request, follow the redirects to get to a page which displays the new order
         response = self.app.post(f'/edit-order/{inserted_order_id}', data=updated_order, follow_redirects = True)
 
+        # assert that the redirect was successful
         self.assertEqual(response.status_code, 200)
 
+        # get the updated order from the db
         updated_order = db_service.db["orders"].find_one({"_id": inserted_order_id})
 
+        # assert that the updated order has the values defined earlier
         self.assertEqual(updated_order["name"], "New Customer")
         self.assertEqual(updated_order["date_of_purchase"], "2024-11-26")
         self.assertEqual(updated_order["pickup_or_delivery"], "pickup")
         self.assertEqual(updated_order["status"], "picked up")
 
+        # check the dashboard to see if the sample order is present
+        response = self.app.get('/')
+        # assert the get request was successful
+        self.assertEqual(response.status_code, 200)
+
+        # check if the updated sample order is present
+        self.assertIn(b"New Customer", response.data)
+        self.assertIn(b"2024-11-26", response.data)
+        self.assertIn(b"pickup", response.data)
+        self.assertIn(b"picked up", response.data)
+
+        # check the order-tracking page for the same thing
+        response = self.app.get('/order-tracking')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"New Customer", response.data)
+        self.assertIn(b"2024-11-26", response.data)
+        self.assertIn(b"pickup", response.data)
+        self.assertIn(b"picked up", response.data)
+
+        # remove the sample drug from the db to avoid filling up the db with vacuous data
         db_service.db["orders"].delete_one({"_id": inserted_order_id})
 
 if __name__ == '__main__':
